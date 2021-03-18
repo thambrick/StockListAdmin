289891ba39e5da536a5df120cdf5a894be5a05b8
treyhambrick <trey.hambrick@cox.net>
2021-03-17 21:29:46 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,254 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//const myfirstelement = <h1>Hello React!...ssss...</h1>
+//ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+//import React, { Component } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './main.css';
+import waitImg from './images/wait.GIF';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//var userList;
+var userId;
+var email;
+var admin="false";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	function wait(onOff){
+	       //alert(onOff);
+		   if (onOff === 'on') { 
+		     document.getElementById('waitDiv2').style.display = "block";
+		     document.body.style.cursor='default';
+		   } else  if (onOff === 'off') { 
+		     document.getElementById('waitDiv2').style.display = "none";
+		     document.body.style.cursor='default';
+		   } 
+       }
+
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+	//let userId2 = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+	//parent.iFrameWin = window;
+
+	let params = window.location.search;
+	userId = params.replace("?userid=", "");
+	//userId = params.substring(0, params.search("&email="));
+	//alert(userId);
+	//this.state = setGetAllUserData("");
+	this.state = {values: []};
+
+	//window.location="https://www.stocklistnow.com/";
+	
+  }
+  
+    componentDidMount() {
+    this.UserList();
+	wait('on');
+  }
+  
+    CallGetUserAPI = (userId)=>{
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            var raw = JSON.stringify({"userId":userId});
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+			//alert("222");alert(raw);
+            fetch("https://0pc218tpdc.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+             .then(response => response.text())
+             .then(result => this.setGetUserData(JSON.parse(result).body))
+            .catch(error => alert(JSON.parse(error).body));
+        }
+		
+
+   setGetUserData(oneUserData){
+			//alert("setGetUserData===");
+			
+			//alert(oneUserData);
+			oneUserData = JSON.parse(oneUserData);
+			admin=oneUserData.Items[0].admin;
+			//alert(admin);
+			//alert(document.getElementById("admin").checked);
+			//document.getElementById("admin").checked=true;
+			//alert(document.getElementById("admin").checked);
+			
+		    if (admin==="true"){
+			   admin="true";
+			   //document.getElementById("admin").checked=true;
+			   this.setState({ checked: true })
+			} else {
+			   admin="false";
+			   //document.getElementById("admin").checked=false;
+			   this.setState({ checked: false })
+			}   
+			//alert("setGetUserData===");
+			//alert(admin);
+			//document.getElementById("admin").checked=true;
+			document.getElementById("email").value=oneUserData.Items[0].email;
+			document.getElementById("displayName").value=oneUserData.Items[0].displayName;
+			document.getElementById("city").value=oneUserData.Items[0].city;
+			document.getElementById("userId").value=oneUserData.Items[0].userId;
+			wait('off');
+		}
+
+  UserList() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({"userId":userId});
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+			//alert("111");alert(raw);
+            fetch("https://ohqt9x52g9.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+             .then(response => response.text())
+			 .then(result => this.setStateNow(JSON.parse(result).body))
+             //.then(result => this.setState(JSON.parse(result).body))
+			 //.then(result => alert(JSON.parse(result).body))
+            .catch(error => alert(JSON.parse(error).body));
+    }
+
+    setStateNow(userData){
+		
+        userData = userData.replace("Items", "values",1);
+		//alert(userData)
+		userData = JSON.parse(userData);
+		//alert(userData.values[0].userId)
+		//callGetAllUsersAPI(userData.values[0].userId);
+		this.CallGetUserAPI(userData.values[0].userId);
+    	this.setState(userData);
+		//wait('off');
+    }
+	
+	deleteProfile = () => {
+		alert("underconstruction");
+	}
+
+    updateProfile = () => {
+	//alert("updated Profile."); 
+    //alert(document.getElementById("displayName").value);	
+    //this.setState({show: false});
+		//let userId = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+		if ((!userId)||(userId==="")){ 
+		   alert("you must login before editing user profiles"); 
+		   return false;
+		   }
+		wait('on');
+		try {
+			//let userId = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+			let userId = document.getElementById("userId").value;
+			//alert(userId);
+			let email = document.getElementById("email").value;
+			let city = document.getElementById("city").value;
+			let displayName = document.getElementById("displayName").value;
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            var raw = JSON.stringify({"userId":userId,"email":email,"displayName":displayName,"city":city,"admin":admin});
+			//alert("444");alert(raw);
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+			fetch("https://u3yyrwv2r5.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+             .then(response => response.text())
+			 //.then(result => alert(JSON.parse(result).body))
+			 .then(result => {wait('off');alert(JSON.parse(result).body)})
+            .catch(error => alert(JSON.parse(error).body));
+		} catch (error) {
+			//console.error(error);
+			alert(error);
+		}
+		
+  }
+
+
+  handleCheckboxChange = event => {
+	//alert("handleCheckboxChange");
+	  
+	//alert(event.target.checked);
+	if (event.target.checked)
+		admin="true";
+	else 
+		admin="false";  
+	  
+	//alert(admin);
+    this.setState({ checked: event.target.checked })
+	
+  }
+  
+   handleChange = (e) => {
+	//alert("handleChange");
+	//alert(e.target.value);
+	this.callGetUserAPI(e.target.value);
+    this.setState({selectedValue: e.target.value})
+  }
+
+/*
+	.ajax-loader {
+		position: absolute;left: 0;top: 0;right: 0;bottom: 0;margin: auto; 
+		}
+		*/
+ 
+  render() {
+    let optionTemplate = this.state.values.map(v => (
+      <option value={v.userId}>{v.displayName}</option>
+    )); 
+	
+	const Checkbox = props => (
+		<input type="checkbox" {...props} />
+	)
+			
+    return (    
+	            <section id="yourProfile" class="wrapper style2 spotlights">
+                <div class="inner">    
+				<div id="waitDiv2" align="center" style={{zIndex:10000}}>
+					<img id='waitImg2' alt="Wait" src={waitImg} class='ajax-loader'  style={{zIndex:10000,position: 'absolute',left: 0,top: 0,right: 0,bottom: 0,margin: 'auto'}}/>
+				</div>
+				<center><h2><label id='profileTitle'>Administration</label></h2></center>
+				<form id='profileForm' method='post' >					
+				<table> 
+				<tr>
+				<td align='right'><label>User</label></td>
+				<td align='left' colspan='2' >
+				<select value={this.state.value} onChange={this.handleChange}>
+					{optionTemplate}
+				</select>
+                </td>
+                </tr>
+				<tr><td align='right'><label>City</label></td><td><input type='text' id='city' onchange={this.handleChange}/></td></tr>
+                <tr><td align='right'><label>Email</label></td><td><input type='text' defaultValue={email} id='email' onchange={this.handleChange}/></td></tr>
+			    <tr><td align='right'><label>Administrator</label></td><td>		
+				<input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked}/>
+				<label>
+				<Checkbox id='admin'
+					checked={this.state.checked}
+					onChange={this.handleCheckboxChange}
+				/>
+				</label>
+
+				<input type='hidden'  id='userId' />
+			    <input type='hidden' id='displayName' />
+
+				</td></tr>
+				<tr><td colspan='2' align='center'>
+				<input type='button' value="Submit" id='updateProfile' onClick={this.updateProfile}/> 
+			    <input type='button' value="Delete User" id='updateProfile' onClick={this.deleteProfile}/></td></tr>
+				</table>
+                </form>
+                </div>				
+				</section> 
+        );
+  }
+}
+
+
+ReactDOM.render(<Container />, document.getElementById('root'));

abd0631dcaf8e0b827216817fe188fbd2b901f62
treyhambrick <trey.hambrick@cox.net>
2021-04-06 14:24:32 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -274,17 +274,17 @@ class Container extends React.Component {
 				<tr>
 				<td align='right' style={{width:'38%'}}><label>Name</label></td>
 				<td align='left' colspan='2' >
-				<select style={{dropdownIndicator:'black',width:'240px',background:'white',color:'rgba(25, 25, 25, 5)'}} value={this.state.value} onChange={this.handleChange}>
+				<select style={{dropdownIndicator:'black',width:'235px',background:'white',color:'rgba(25, 25, 25, 5)'}} value={this.state.value} onChange={this.handleChange}>
 					{optionTemplate}
 				</select>
                 </td>
                 </tr>
-				<tr><td align='right' style={{width:'38%'}}><label>City</label></td><td><input style={{color:'rgba(25, 25, 25, 5)',width:'240px'}} id='city' onchange={this.handleChange}/></td></tr>
-                <tr><td align='right' style={{width:'38%'}}><label>Email</label></td><td><input style={{color:'rgba(25, 25, 25, 5)',width:'240px'}} defaultValue={email} id='email' onchange={this.handleChange}/></td></tr>
+				<tr><td align='right' style={{width:'38%'}}><label>City</label></td><td><input style={{color:'rgba(25, 25, 25, 5)',width:'235px'}} id='city' onchange={this.handleChange}/></td></tr>
+                <tr><td align='right' style={{width:'38%'}}><label>Email</label></td><td><input style={{color:'rgba(25, 25, 25, 5)',width:'235px'}} defaultValue={email} id='email' onchange={this.handleChange}/></td></tr>
 			    <tr><td align='right' style={{width:'38%',}}><label>ID</label></td><td><label id='userId'></label></td></tr>
 			    
 				<tr><td align='right'style={{width:'38%',textAlignVertical:'top',color:'rgba(25, 25, 25, 5)'}}><label>Admin</label></td><td>		
-				<input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked}/>
+				<input type="checkbox" style={{textAlignVertical:'bottom'}}onChange={this.handleCheck} defaultChecked={this.state.checked}/>
 				<label>
 				<Checkbox id='admin'
 					checked={this.state.checked}
